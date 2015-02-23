26e6f507d3bad1e569d4f9440954163c5744ddb1
Ernesto <ernesto@landcorpinternational.com>
2015-02-23 10:51:31 +0100

    Added 'js' task and runs minify CSS function.

--- gulpfile.js
+++ gulpfile.js
@@ -14,21 +14,29 @@ var livereload = require('gulp-livereload');
 var sourceSASS = './scss';
 var targetSASS = 'public/css';
 
-var sourceJS = 'source/js';
-var targetJS = 'public/js';
+var sourceJS = 'js/source';
+var targetJS = 'js';
 
 
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
         .pipe(sass())
+        .pipe(minifycss())
         .pipe(gulp.dest('./css'));
 });
 
-
+gulp.task('js', function() {
+    gulp.src(sourceJS + '/app.js')
+        .pipe(concat("app.min.js"))
+        .pipe(uglify({mangle: true}).on('error', gutil.log))
+        .pipe(gulp.dest(targetJS));
+});
 
 gulp.task('watch', function() {
   gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('./js/source/*.js', ['js']);
+
 });
 
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch', 'js']);
